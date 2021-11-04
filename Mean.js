645fe1158dba7aee47b9038c3534cf771fa6e82d
Hernán D. Mercado <hernandmf@gmail.com>
2021-11-04 13:52:07 -0500

    Function to calculate Mean

---
+++ Mean.js
@@ -0,0 +1,29 @@
+function calcularMediana(lista = [100, 200, 300, 400, 500, 600, 700, 800]) {
+  function average(lista = [1, 2, 3, 4]) {
+    let sumaLista = 0;
+    let promedio = 0;
+    // for (let i = 0; i < lista.length; i++) {
+    //   sumaLista = lista[i] + sumaLista;
+    //   promedio = sumaLista / lista.length;
+    // }
+    sumaLista = lista.reduce((acum, elemt) => acum + elemt);
+    promedio = sumaLista / lista.length;
+    return promedio;
+  }
+  let media;
+  function esPar(num) {
+    media = parseInt(lista.length / 2);
+    if (num % 2 === 0) {
+      const numero1 = lista[media - 1];
+      const numero2 = lista[media];
+      console.log(numero1, numero2);
+      const mediana = average([numero1, numero2]);
+      console.log(mediana);
+    } else {
+      console.log(lista[media]);
+    }
+  }
+  esPar(lista.length);
+}
+
+calcularMediana();
